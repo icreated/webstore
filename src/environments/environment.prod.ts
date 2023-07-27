ee2c084b628a8dad48b8cf8ed17e1e8c39f6d6f8
Sergey Polyarus <macspok@macbook-pro-de-sergey-1.home>
2023-07-28 00:01:41 +0200

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,9 @@ export const environment = {
     production: true,
     api: {
       baseUrl: 'https://webstore-json-server.icreated.co',
-    }
+    },
+  whitelistedDomains: ['webstore-json-server.icreated.co'],
+  storageKey: 'webStore',
+  defaultCountryId: 100,
+  webmasterEmail: 'webmaster@amazzing-company.comm',
 };
